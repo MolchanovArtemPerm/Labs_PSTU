501f15bd9da312326ca1d26787996a6a80d6cfac
ArtemMolchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2021-10-28 14:10:29 +0500

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
 #include <ctime>
-
 using namespace std;
-
 int main()
 {
     srand(time(0));
-
-    const int trypoint = rand() % 100;
+    const int number = rand() % 100;
     const int maxpoint = 6;
-
     cout << "Угадайте загаданное число в диапозоне от 1 до 100" << endl;
-
-    int maxpoints = 0;
+    int point = 0;
     int trys = 0;
-
-    while(maxpoints < maxpoint && trys!= trypoint)
+    while(point < maxpoint && trys!= number)
     {
-        cout << maxpoints + 1 << ". Введите число: " << endl;
-
+        cout << point + 1 << ". Введите число: " << endl;
         cin >> trys;
-
-        if(trys != trypoint)
-            cout << "Загаданное число " << ((trys < trypoint) ? "больше" : "меньше") << " " << trys << endl;
-
-        maxpoints++;
-    }
-
-    if(trys == trypoint)
+        if(trys != number)
         {
-            cout << "С " << maxpoints << " Попытки" <<
-            " Вы смогли отгадать загаданное число: " << trypoint << endl;
+            cout << "Загаданное число " << ((trys < number) ? "больше" : "меньше") << " " << trys << endl;
+        }
+        point = point + 1;
+    }
+    if(trys == number)
+        {
+            cout << "С " << point << " Попытки" <<
+            " Вы смогли отгадать загаданное число: " << number << endl;
         }
     else
        {
-           cout << "за отведенные " << maxpoint << " попыток Вы не смогли отгадать число: " 
-            << trypoint << endl;
+           cout << "За отведенные " << maxpoint << " попыток Вы не смогли отгадать число: " 
+            << number << endl;
        }
 }
