073c505f54df0f13b7e85c04481f006f1944e5f8
ArtemMolchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2021-10-28 14:04:00 +0500

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,66 +1,64 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-int main()
-{
-    string number = "";
-
-    bool f = false;
-    
+int main() {
     setlocale(LC_ALL, "ru");
-
+    string number;
+    bool f = false;
     do {
         cout << "Введите число от 100 до 999: " << endl;
-
         cin >> number;
-
         if(number[0] != '0' && number.length() == 3)
         {
-            f = number[0] != number[1] && number[0] != number[2] && number[1] != number[2];
-
+            f = true;
         }
-
         if(!f)
-            cout << "Число " << number
-                        << " не удовлетворяет условию. Повторите с другим числом." << endl;
-    } while(!f);
-
-    int max = 0;
-    string s;
-    for(int i = 0, a; i < number.length(); i++)
-    {
-        if(number[i] != '0')
-            s.append(number.substr(i, 1));
-
-        for(int j = 0; j < number.length(); j++)
         {
-
-            if(number[i] != number[j]) {
-                s.append(number.substr(j, 1));
-
-                for(int x = 0; x < number.length(); x++)
-                {
-                    if(number[i] != number[x] && number[j] != number[x])
-                    {
-                        s.append(number.substr(x, 1));
-
-                        cout << s << endl;
-
-                        if(max < atoi(s.c_str()))
-                            max = atoi(s.c_str());
-
-                        s.erase(s.length() - 1);
-                    }
-                }
-
-                s.erase(s.length() - 1);
-            }
+           cout << "Число " << number << " не удовлетворяет условию. Повторите с другим числом." << endl; 
         }
-
-        s.clear();
+    }while (!f);
+    int a = stoi(number) / 100;
+    int b = ((stoi(number)) / 10) % 10;
+    int c = stoi(number) % 10;
+    if ((a != b) && (b != c) && (a != c))
+    {
+        cout << a << b << c << endl;
+        cout << a << c << b << endl;
+        cout << b << a << c << endl;
+        cout << b << c << a << endl;
+        cout << c << a << b << endl;
+        cout << c << b << a << endl;
     }
-
-    cout << "Максимальное число: " << max << endl;
-    return 0;
+    if ((a > b) && (a > c)) 
+		{
+			if (b > c) { 
+				cout << "Максимальное число:  " << a << b << c << endl; 
+			}
+			else
+			{
+				cout << "Максимальное число: " << a << c << b << endl;	
+			}
+		}
+	if ((b > a) && (b > c)) 
+	{
+		if (a > c) 
+		{
+			cout << "Максимальное число: " << b << a << c << endl; 
+		}
+		else
+		{
+			cout << "Максимальное число: " << b << c << a << endl;
+		}
+	}
+	if ((c > a) && (c > b)) 
+	{
+		if (a > b) 
+		{
+			cout << "Максимальное число: " << c << a << b << endl;
+		}
+		else		
+		{
+		cout << "Максимальное число: " << c << b << a << endl;
+		}
+	}
 }
