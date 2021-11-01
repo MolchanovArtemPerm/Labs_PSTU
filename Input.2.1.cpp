0a2e38480a9af0726a335c607521bee08bd924a5
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2021-11-01 20:07:24 +0500

    Add files via upload

---
+++ Input.2.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout << "¬ведите два числа: " << endl;
+    cin >> a;
+    cin >> b;
+    cout << a * b;
+}
---
+++ Input.2.2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e;
+    cout << "¬ведите п€ть чисел: " << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cout << e << endl << d << endl << c << endl << b << endl << a;
+}
