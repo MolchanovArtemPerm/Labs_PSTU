0dbcafa24b0b9c7b607e80de68b8308e6d5a04e1
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2021-11-01 19:58:09 +0500

    Rename 2.2.cpp to Structure2.2.cpp

---
+++ Structure2.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const double g = 7.481;
+    int f;
+    cout << "Ââåäèòå ÷èñëî ãàëëîíîâ: " << endl;
+    cin >> f;
+    cout << "Îáúåì " << f << " ãàëëîíîâ â êóáè÷åñêèõ ôóòàõ = " << f / g;
+}
