d717131737f8e9f57469bedc0a068661c9cf62fe
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2022-02-12 17:11:20 +0500

    Create 9-ElementZ.cpp

---
+++ 9-ElementZ.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int size,z,p;
+void delElement(int *a,int size){
+  int k,c;
+  bool f = false;
+  cout<<"Введите элемент который нужно удалить: ";
+  cin>>z;
+  for (int i = 0; !f ; i++)
+  {
+    if (a[i] == z){
+      k = i;
+      f = true;
+    }
+  }
+  for(int i = k; i < size-1; i++){
+    a[i]=a[i+1];
+  }
+  for (int i = 0; i < size-1 ; i++){
+    cout << a[i] << " ";
+  }
+  cout<<endl<<endl<<"Введите индекс на который нужно вставить элемент: "; 
+  cin>>p;
+  a[p]=z;
+  for (int i = 0; i < size-1 ; i++){
+    cout << a[i] << " ";
+  }
+}
+int main()
+{
+  setlocale(LC_ALL,"Rus");
+  srand(time(0));
+  cout<<"Размер массива: "; 
+  cin>>size;
+  int *a = new int[size];
+  for (int i = 0; i < size; i++){
+    a[i] = rand() % 100;
+    cout << a[i] << " ";
+  }
+  cout<<endl<<endl;
+  delElement(a,size);
+  return 0;
+}
