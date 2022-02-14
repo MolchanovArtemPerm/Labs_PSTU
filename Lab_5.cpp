7f75adcf2ba3945b087576658f02550491a4f082
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2022-02-14 23:04:30 +0500

    Update Lab_5.cpp

--- Lab_5.cpp
+++ Lab_5.cpp
@@ -2,35 +2,33 @@
 using namespace std;
 int main()
 {
-   const int n = 4;
-   int a[n][n];
+  const int n = 5;
+  int a[n][n];
  
-   srand(time(0));
-   for(int i = 0; i < n; ++i)
-   {
-      for(int j = 0; j < n; ++j)
-      {
-         a[i][j] = rand() % 101;
-         cout.width(5);
-         cout << a[i][j];
-      }
-      cout << endl;
-   }
-   
-   for(int j = 1; j < n; ++j)
-   {
-      double sum = 0;
-      for(int i = 0; i < n; ++i)
-         sum += a[i][j-1];
-      double average = sum / n;
- 
-      int count = 0;
-      for(int i = 0; i < n; ++i)
-         if(a[i][j] > average)
-            ++count;
- 
+  srand(time(0));
+  for(int i = 0; i < n; ++i)
+  {
+    for(int j = 0; j < n; ++j)
+    {
+      a[i][j] = rand() % 101;
+      cout.width(5);
+      cout << a[i][j];
+    }
+    cout << endl;
+  }
+  for(int j = 1; j < n; ++j)
+  {
+    double sum = 0;
+    for(int i = 0; i < n; ++i)
+      sum += a[i][j-1];
+    double average = sum / n;
+    int count = 0;
+    for(int i = 0; i < n; ++i)
+      if(a[i][j] > average)
+        ++count;
       cout << "среднее арифметическое элементов " << j << "-го столбца: " << average;
       cout << endl << "количство элементов " << j + 1 << "-го столбца больших " << average;
       cout << ": " << count << endl << endl;
    }
+  return 0;
 }
