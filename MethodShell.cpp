49d68e20dad1457db3763830f3904bbc0ca2fdd5
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2022-02-14 18:13:05 +0500

    Update MethodShell.cpp

--- MethodShell.cpp
+++ MethodShell.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 using namespace std;
-int shell(int *a,int size) {
-	int t;
-	int d = (size+1) / 2;
-	while (d > 0) {
-		for (int i = 0; i < size - d; i++) {
-			if (a[i] > a[i + d]) {
-				t = a[i + d];
-				a[i + d] = a[i];
-				a[i] = t;
-			}
-		}
-		d = d / 2;
-	}
+void shellsort(int *a, int size)
+{
+  int i,j,t;
+  int d=size/2;
+  while (d>0)
+  {
+    for (i=0; i<size-d; i++)
+    {
+      j=i;
+      while (j>=0)
+      {
+        if(a[j]>a[j+d]){
+          t=a[j];
+          a[j]=a[j+d];
+          a[j+d]=t;
+        }
+        j--;
+      }
+    }
+    d=d/2;
+  }
   cout << endl << endl;
-	for (int i = 0; i < size; i++)
-		cout << a[i] << ' ';
-	return 0;
+  for (i=0; i<size; i++) 
+   cout<<a[i]<<" ";
 }
 int main()
 {
 	setlocale(LC_ALL, "ru");
   srand(time(0));
-	int const size = 7;
+	int const size = 5;
 	int* a = new int[size];
 	for (int i = 0; i<size; i++){
     a[i] = rand() % 101;
     cout<<a[i] << " ";
   }
-	shell(a, size);
+	shellsort(a, size);
 	delete[] a;
 	return 0;
 }
