2bb48be37737b8430d05d78d0f98effd7758b30c
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2022-02-16 13:10:01 +0500

    Add files via upload

---
+++ lab4.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+int size = 5;
+void del(int *steck,int size)
+{
+  for(int i = size-1; i < size-1; i++)
+  {
+    steck[i]=steck[i+1];
+  }
+  cout << endl << "Стек состоит из:" << endl;
+  for (int i = 0; i < size-1 ; i++)
+  {
+    cout << steck[i] << " ";
+  }
+}
+
+void dob(int *steck,int size)
+{
+  int temp,zam;
+  cout << endl << "Введите число для добавления в стек: " << endl;
+  cin >> temp;
+  for(int i = size; i > 0; i--)
+  {
+    steck[i] = steck[i-1];
+  }
+  steck[0] = temp;
+  cout << endl << "Стек состоит из:" << endl;
+  for (int i = 0; i < size ; i++)
+  {
+    cout << steck[i] << " ";
+  }
+}
+
+int main()
+{
+  setlocale(LC_ALL,"Rus");
+  int* steck = new int[size];
+  int s,e;
+  bool f = true;
+  cout << "Введите элемент для заполнение стека" << endl;
+  for (int i = 0; i < size; ++i) 
+  {
+    cin >> e;
+    steck[i] = e;
+  } 
+  cout << endl << "Стек состоит из: " << endl;
+  for (int i = 0; i < size; ++i) 
+  {
+    cout << steck[i] << " ";
+  }
+  while(f)
+  {
+    cout << endl << endl
+    << "Выберете операцию:\n"
+    << "1. Удаление элемента\n"
+    << "2. Добавление элемента\n"
+    << "3. Остановка" << endl;
+    cout << "Ваш выбор: ";
+    cin >> s;
+    switch(s)
+    {
+      case 1:
+      {
+        del(steck,size);
+        size--;
+        break;
+      }
+      case 2:
+      {
+        size++;
+        dob(steck,size);
+        break;
+      }
+      case 3: 
+      {
+        f = false; 
+        break;
+      }
+      default: 
+      {
+      cout << "ERORR" << endl << "Введите значение из списка " ;
+      }
+    }
+  }
+  return 0;
+}
