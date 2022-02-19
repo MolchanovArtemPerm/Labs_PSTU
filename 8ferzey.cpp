f51908622234cc4ea39f89f2d0c76b967a19ebec
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2022-02-19 16:23:29 +0500

    Update 8ferzey.cpp

--- 8ferzey.cpp
+++ 8ferzey.cpp
@@ -1,78 +1,51 @@
 #include <iostream>
 using namespace std;
 int board[8][8];
-void set(int i, int j)
+int check(int a, int b)
 {
-  for (int x = 0; x < 8; ++x)
+  for(int i = 0; i < 8; ++i)
   {
-    ++board[x][j];
-    ++board[i][x];
-    int diag;
-    diag = j - i + x;
-    if (diag >= 0 && diag < 8)
-      ++board[x][diag];
-    diag = j + i - x;
-    if (diag >= 0 && diag < 8)
-      ++board[x][diag];
+    if (board[i][b])
+      return 0;
+    if (b-a+i>=0)
+      if(board[i][b-a+i])
+        return 0;
+    if (b+a-i<8)
+      if (board[i][b+a-i])
+        return 0;
   }
-  board[i][j] = -1;
+  return 1;
 }
-
-void reset(int i, int j)
+ 
+int vert(int i)
 {
-  for (int x = 0; x < 8; ++x)
+  if (i == 8)
+    return 1;
+  for(int j = 0; j < 8; ++j)
   {
-    --board[x][j];
-    --board[i][x];
-    int diag;
-    diag = j - i + x;
-    if (diag >= 0 && diag < 8)
-      --board[x][diag];
-    diag = j + i - x;
-    if (diag >= 0 && diag < 8)
-      --board[x][diag];
-  }
-  board[i][j] = 0;
-}
-
-bool tryq(int i)
-{
-  bool result = false;
-  for (int j = 0; j < 8; ++j)
-  {
-    if (board[i][j] == 0)
+    if (check(i,j))
     {
-      set(i,j);
-      if (i == 7)
-	      result = true;
-      else
-      {
-      	if (!(result = tryq(i + 1)))
-      	  reset(i, j);
-      }
-    }
-    if (result)
-      break;
-  }
-  return result;
-}
-
-int main()
-{
-  for (int i = 0; i < 8; ++i)
-    for (int j = 0; j < 8; ++j)
+      board[i][j] = 1;
+      if (vert(i+1))
+        return 1;
       board[i][j] = 0;
-  tryq(0);
-  for (int i = 0; i < 8; ++i)
-  {
-    for (int j = 0; j < 8; ++j)
-    {
-      if (board[i][j] == -1)
-      	cout << " w ";
-      else
-      	cout << " - ";
     }
-    cout << endl;
   }
   return 0;
+}
+ 
+int main()
+{
+  vert(0);
+  for(int i = 0; i< 8; ++i)
+  {
+    for(int j = 0; j< 8; ++j)
+    {
+      if (board[i][j] == 0)
+        cout << "- ";
+      else if (board[i][j] == 1)
+        cout << "w ";
+    }
+  cout << endl;
+  }
 }
