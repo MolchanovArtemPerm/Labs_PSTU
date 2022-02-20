5de51a3c12c1f1d6648d10a2cc8293bb48a44bba
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2022-02-20 12:09:57 +0500

    Update HanoyskayaTower.cpp

--- HanoyskayaTower.cpp
+++ HanoyskayaTower.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
 void move(int st1, int st2)
-{
-  cout << st1 << " => " << st2 << endl;
+{ 
+  cout << st1 << " => " << st2 << endl; 
 }
-void moveT(int count,int st1,int st2,int temp)
+void moveT(int count, int st1, int st2, int st3)
 {
   if (count != 0)
   {
-  moveT(count-1,st1,temp,st2);
-  move(st1,st2);
-  moveT(count-1,temp,st2,st1);
+    moveT(count - 1, st1, st3, st2);
+    move(st1, st2);
+    moveT(count - 1, st3, st2, st1);
   }
 }
-int main()
-{
-  moveT(3,1,2,3);
+int main() {
+  moveT(3, 1, 2, 3);
   return 0;
 }
