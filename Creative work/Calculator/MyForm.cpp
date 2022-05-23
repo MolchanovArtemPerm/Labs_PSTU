e70d0297df4dc64038ed8b794f7bbbb9dc0c793e
Artem Molchanov <83158979+MolchanovArtemPerm@users.noreply.github.com>
2022-05-23 22:39:34 +0500

    Delete Creative work/Calculator directory

--- Creative work/Calculator/MyForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm.h"
-using namespace calcLog;
-int main()
-{
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MyForm);
-	return 0;
-}
